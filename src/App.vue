54f5b95d412976ce34c9fd9bb3f25971ba26ad90
Adem Kolliari <ademk95@hotmail.co.uk>
2019-11-22 08:25:32 +0600

    my-navbar component created

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <b-navbar toggleable="lg" type="dark" variant="info" fixed="top">
-        <b-navbar-brand href="#">ad3mk95</b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <!-- <b-nav-item href="#">Link</b-nav-item> -->
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <template v-if="!isLoggedIn">
-              <b-nav-item to="/login">Login</b-nav-item>
-              <b-nav-item to="/register">Register</b-nav-item>
-            </template>
-
-            <template v-else>
-              <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template v-slot:button-content>
-                  <em>{{ loggedUserName }}</em>
-                </template>
-                <b-dropdown-item hre="#" @click="goToDashboard">Dashboard</b-dropdown-item>
-                <b-dropdown-item href="#" @click="logoutNow">Log Out</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </template>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+    <my-navbar></my-navbar>
     <router-view />
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-
 export default {
   data() {
     return {};
-  },
-  computed: {
-    // get store data
-    ...mapGetters({
-      isLoggedIn: "getIsLoggedIn",
-      loggedUserName: "getLoggedUserName"
-    })
-  },
-  methods: {
-    // map store action/methods
-    ...mapActions({
-      logout: "logout"
-    }),
-
-    goToDashboard() {
-      if (this.$route.name !== "dashboard") {
-        this.$router.push("/dashboard");
-      }
-    },
-
-    logoutNow() {
-      this.logout();
-      this.$router.push("/login");
-    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import App from "./App.vue";
+import MyNavbar from "./components/MyNavbar";
 import MyModal from "./components/MyModal";
 import MyRating from "./components/MyRating";
 import router from "./router";
@@ -10,6 +11,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/css/style.css";
 
 Vue.use(BootstrapVue);
+Vue.component("my-navbar", MyNavbar);
 Vue.component("my-modal", MyModal);
 Vue.component("my-rating", MyRating);
 
